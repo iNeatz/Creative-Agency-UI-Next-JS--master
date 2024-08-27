d9befaa174df6d6016dc3a088502169101a4c755
iNeatz <sahilshrestha77@gmail.com>
2024-08-27 20:19:31 +0545

    patch v0.2

--- components/router.js
+++ components/router.js
@@ -3,6 +3,5 @@ import Banner from "./Banner"
 import Testimonial from "./Testimonial"
 import ShowCase from "./ShowCase"
 import Brand from "./Brand"
-import BlogCard from "./BlogCard"
 
 export { Expertise, Banner, Testimonial, ShowCase, Brand, BlogCard }
