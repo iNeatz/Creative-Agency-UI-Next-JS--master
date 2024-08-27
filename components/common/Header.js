4aaa3891bed783ce7bf1e7ba6520b0e8bf49a05b
iNeatz <sahilshrestha77@gmail.com>
2024-08-27 19:47:31 +0545

    Header and Hero edit done

---
+++ components/common/Header.js
@@ -0,0 +1,73 @@
+import Link from 'next/link'
+import { TitleLogo } from './Title'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { RiMenu4Line, RiPhoneFill } from 'react-icons/ri'
+import { AiOutlineClose } from 'react-icons/ai'
+
+const Header = () => {
+	const [activeLink, setActiveLink] = useState('')
+	const [open, setOpen] = useState(false)
+
+	const router = useRouter()
+	useEffect(() => {
+		setActiveLink(router.pathname)
+	}, [router.pathname])
+	return (
+		<>
+			<header>
+				<div className='container'>
+					<div className='logo'>
+						<Link href='/'>
+							<TitleLogo title='creative' caption='7' className='logomin' />
+						</Link>
+					</div>
+					<nav
+						className={open ? 'openMenu' : 'closeMenu'}
+						onClick={() => setOpen(null)}
+					>
+						<Link
+							href='/'
+							className={activeLink == '/' ? 'activeLink' : 'none'}
+						>
+							Home
+						</Link>
+						<Link
+							href='/agency'
+							className={activeLink == '/agency' ? 'activeLink' : 'none'}
+						>
+							Agency
+						</Link>
+						<Link
+							href='/team'
+							className={activeLink == '/team' ? 'activeLink' : 'none'}
+						>
+							Team
+						</Link>
+						<Link
+							href='/services'
+							className={activeLink == '/services' ? 'activeLink' : 'none'}
+						>
+							Services
+						</Link>
+						<Link
+							href='/showcase'
+							className={activeLink == '/showcase' ? 'activeLink' : 'none'}
+						>
+							Showcase
+						</Link>
+						<button className='button-primary cta-butn'>
+							<RiPhoneFill />
+							<span>+977 984-8578-704</span>
+						</button>
+					</nav>
+					<button className='menu-toggler' onClick={() => setOpen(!open)}>
+						{open ? <AiOutlineClose size={25} /> : <RiMenu4Line size={25} />}
+					</button>
+				</div>
+			</header>
+		</>
+	)
+}
+
+export default Header
